7ea4baec0fb35408e549253c669fa969436a10e7
Muash <muazwazir@gmail.com>
2025-07-16 01:47:07 +0800

    feat: API error builder and resolver

--- opendosm-api/util/error.go
+++ opendosm-api/util/error.go
@@ -1,5 +1,11 @@
 package util
 
+import (
+	"errors"
+	"goyave.dev/goyave/v5"
+	"goyave.dev/goyave/v5/slog"
+)
+
 type ErrorResponse struct {
 	HttpCode int    `json:"http_code"`
 	Message  string `json:"message"`
@@ -14,4 +20,12 @@ func BuildError(httpCode int, message string) error {
 		HttpCode: httpCode,
 		Message:  message,
 	}
+}
+
+func HandleError(resp *goyave.Response, log *slog.Logger, err error) {
+	var errData *ErrorResponse
+	if !errors.As(err, &errData) {
+		log.Warn("invalid error data")
+	}
+	resp.JSON(errData.HttpCode, errData)
 }
